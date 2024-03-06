72a8d447f75eccf74d7dc43630f7c52e7a719b56
Fareed <fareedsayed95@gmail.com>
2024-03-07 02:20:56 +0530

    done backend setup using vite

--- campussory/backend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics
-from .serializers import EventSerializer
-from .models import Client
-# Create your views here.
-class index(generics.CreateAPIView):
-    queryset = Client.objects.all()
-    serializer_class = EventSerializer
