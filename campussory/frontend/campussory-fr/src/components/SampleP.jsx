0cd5d7448c599bbb1696b8ec83ed97e99163cb03
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2024-03-06 20:42:56 +0530

    basic 4 (#7)
    * setup
    
    * setup 2
    
    * basic
    
    * basic changes changes in frontend

---
+++ campussory/frontend/campussory-fr/src/components/SampleP.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import sample_img1 from '/public/undraw_certification_re_ifll.svg'
+import '/src/Styles/samplem.css'
+function SampleP() {
+  return (
+    <div className='samplep'>
+<img className="vector" src={sample_img1}></img>     
+SAMPLE PAPER
+    </div>
+  )
+}
+
+export default SampleP
