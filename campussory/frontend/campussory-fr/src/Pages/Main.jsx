6de1dd1ff62c498faac754dfc98aecf0e3f77961
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-03-07 00:10:18 +0530

    css left

--- campussory/frontend/campussory-fr/src/Pages/Main.jsx
+++ campussory/frontend/campussory-fr/src/Pages/Main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import Map from '/src/components/Map.jsx'
 import Placed from '../components/Placed'
+import Hero from '/src/components/Hero.jsx'
 function Main() {
   return (
     <div>
+      <Hero></Hero>
       <Map></Map>
       <Placed></Placed>
 
--- campussory/frontend/campussory-fr/src/components/Placed.jsx
+++ campussory/frontend/campussory-fr/src/components/Placed.jsx
@@ -5,6 +5,7 @@ import '/src/Styles/Placed.css';
 function Placed() {
 
   const placedStudents = [
+   
     {
       img: 'path_to_image1.jpg',
       name: 'John Doe',
@@ -18,6 +19,48 @@ function Placed() {
       dis: 'Electrical Engineering',
       comp: 'XYZ Corporation',
       amt: '$65,000',
+    },
+    {
+      img: 'path_to_image1.jpg',
+      name: 'John Doe',
+      dis: 'Computer Science',
+      comp: 'ABC Technologies',
+      amt: '$60,000',
+    },
+    {
+      img: 'path_to_image2.jpg',
+      name: 'Jane Smith',
+      dis: 'Electrical Engineering',
+      comp: 'XYZ Corporation',
+      amt: '$75,000',
+    },
+    {
+      img: 'path_to_image3.jpg',
+      name: 'Alice Johnson',
+      dis: 'Data Science',
+      comp: '123 Enterprises',
+      amt: '$80,000',
+    },
+    {
+      img: 'path_to_image4.jpg',
+      name: 'Bob Williams',
+      dis: 'Software Engineering',
+      comp: 'Tech Solutions Inc.',
+      amt: '$70,000',
+    },
+       {
+      img: 'path_to_image3.jpg',
+      name: 'Alice Johnson',
+      dis: 'Data Science',
+      comp: '123 Enterprises',
+      amt: '$80,000',
+    },
+    {
+      img: 'path_to_image4.jpg',
+      name: 'Bob Williams',
+      dis: 'Software Engineering',
+      comp: 'Tech Solutions Inc.',
+      amt: '$70,000',
     },
 
   ];
