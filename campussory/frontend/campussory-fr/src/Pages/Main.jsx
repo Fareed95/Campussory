2ac4fa66d08054de9d8a849f78231e5ee243fe40
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2024-03-06 16:46:52 +0530

    setup 2 (#3)
    * setup
    
    * setup 2

---
+++ campussory/frontend/campussory-fr/src/Pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '/frontend/campussory-fr/src/components/Navbar.jsx'
+function Main() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Main
