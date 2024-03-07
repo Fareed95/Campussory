3daca9d4c4b05a79dd9067f3a6ec86ff81ce56b0
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-03-07 11:19:33 +0530

    last fronted

--- campussory-fr/src/components/SampleP.jsx
+++ campussory-fr/src/components/SampleP.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import sample_img1 from '/public/undraw_certification_re_ifll.svg'
 import '/src/Styles/samplem.css'
+import { Link } from "react-router-dom";
+
 function SampleP() {
   return (
     <div className='samplep'>
 <img className="vector" src={sample_img1}></img>     
-<h1>SAMPLE PAPER</h1>
+<Link to = "/Samplepaper"><h2>Sample paper</h2></Link>
     </div>
   )
 }
