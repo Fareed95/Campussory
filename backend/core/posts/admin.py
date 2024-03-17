bc7b5f7d502f9014e841163619b9669ce1564f7f
Fareed Khalil Sayed <143815597+Fareed95@users.noreply.github.com>
2024-03-17 06:10:59 +0530

    updated models (#21)

--- backend/core/posts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Client, FormData, FormDataCompany
-# Register your models here.
-admin.site.register(Client)
-admin.site.register(FormData)
-admin.site.register(FormDataCompany)
--- backend/posts/api/serializers.py
+++ backend/posts/api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from ..models import Client, FormData, FormDataCompany
+from ..models import Client, FormData, FormDataCompany, Company
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta :
         model = Client 
+        fields = ('name','variable1','variable2','variable3')
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Company 
         fields = ('name','variable1','variable2','variable3')
 
 
